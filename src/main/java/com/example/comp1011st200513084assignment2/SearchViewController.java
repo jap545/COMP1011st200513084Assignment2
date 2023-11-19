dedaaa4fdaf8029638ac822ce488175c751679f7
jap545 <japneeths@icloud.com>
2023-11-19 14:11:22 -0500

    Added search-view.fxml and SearchViewController and made a basic skeleton to use

--- src/main/java/com/example/comp1011st200513084assignment2/SearchViewController.java
+++ src/main/java/com/example/comp1011st200513084assignment2/SearchViewController.java
@@ -1,4 +1,56 @@
 package com.example.comp1011st200513084assignment2;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 public class SearchViewController {
+
+    @FXML
+    private ListView<?> listView;
+
+    @FXML
+    private Label msgLabel;
+
+    @FXML
+    private ImageView posterImageView;
+
+    @FXML
+    private ProgressBar progressBar;
+
+    @FXML
+    private HBox resultsBox;
+
+    @FXML
+    private Label resultsMsgLabel;
+
+    @FXML
+    private TextField searchTextField;
+
+    @FXML
+    private VBox selectedVBox;
+
+    @FXML
+    private VBox titlesVBox;
+
+    @FXML
+    private void initialize()
+    {
+        progressBar.setVisible(false);
+        selectedVBox.setVisible(false);
+        titlesVBox.setVisible(false);
+        msgLabel.setVisible(false);
+    }
+    @FXML
+    private void searchForTeams(ActionEvent event)
+    {
+        String teamName = searchTextField.getText().trim();
+    }
+
 }
