6fd40926f49956074ba7809a7a0a3d6ece7ba1e1
jap545 <japneeths@icloud.com>
2023-11-24 21:09:07 -0500

    Created stats-view.fxml and StatsViewController.java

---
+++ src/main/java/com/example/comp1011st200513084assignment2/StatsViewController.java
@@ -0,0 +1,81 @@
+package com.example.comp1011st200513084assignment2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class StatsViewController {
+
+    @FXML
+    private Label assistsLabel1;
+
+    @FXML
+    private Label assistsLabel2;
+
+    @FXML
+    private Label blocksLabel1;
+
+    @FXML
+    private Label blocksLabel2;
+
+    @FXML
+    private Label fastBreakPointsLabel1;
+
+    @FXML
+    private Label fastBreakPointsLabel2;
+
+    @FXML
+    private ImageView imageView1;
+
+    @FXML
+    private ImageView imageView2;
+
+    @FXML
+    private Label pointsInPaintLabel1;
+
+    @FXML
+    private Label pointsInPaintLabel2;
+
+    @FXML
+    private Label pointsLabel1;
+
+    @FXML
+    private Label pointsLabel2;
+
+    @FXML
+    private Label pointsOffTurnoversLabel1;
+
+    @FXML
+    private Label pointsOffTurnoversLabel2;
+
+    @FXML
+    private Label reboundsLabel1;
+
+    @FXML
+    private Label reboundsLabel2;
+
+    @FXML
+    private Label stealsLabel1;
+
+    @FXML
+    private Label stealsLabel2;
+
+    @FXML
+    private Label teamLabel1;
+
+    @FXML
+    private Label teamLabel2;
+
+    @FXML
+    private Label turnoversLabel1;
+
+    @FXML
+    private Label turnoversLabel2;
+
+    @FXML
+    void backToSearch(ActionEvent event) {
+
+    }
+
+}
